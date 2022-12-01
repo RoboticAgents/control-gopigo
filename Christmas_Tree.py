ea20fbb82422face1e180fb17a15f8dda000e761
cheng2 <cheng2@allegheny.edu>
2022-12-01 08:02:42 -0500

    adding new version

--- Christmas_Tree.py
+++ Christmas_Tree.py
@@ -1,1 +1,55 @@
-Sequence = [ ]
+# import easygopigo3 as easy
+# import time
+# import pygame
+
+# my_gopigo = easy.EasyGoPiGo3()
+
+
+def command(inputs):
+    if inputs == 'w':
+        # gpg.drive_inches(5)
+        print("going forward")
+        
+    if inputs == 's':
+
+        print("going backward")
+
+    if inputs == 'q':
+
+        print("turn left slightly")
+
+    if inputs == 'e':
+        print("turn right slightly")
+
+    if inputs == 'a':
+        print("turn left 90 degree")
+
+    if inputs == 'd':
+        print("turn right 90 degree")
+
+    if inputs == 'z':
+        print("turn left x degree")
+
+    if inputs == 'c':
+        print("turn right x degree")
+
+
+
+def main():
+
+    mylist = []
+
+    while True:
+        inputs = input("Enter command here: ")
+        print("this is your input:", inputs)
+
+        if inputs == "":
+            print("breaking process")
+            break
+
+        mylist.append(inputs)
+        command(mylist[0])
+        print(mylist)
+        mylist.clear()
+
+main()
